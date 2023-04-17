c6d030946f661083a0b0ae3b53b6b1b083423ff2
lawilmoth <lwilmoth@eriesd.org>
2023-04-17 09:32:29 -0400

    boilerplate is done

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,9 +7,26 @@ Page 227 of the book.agee 265 of the PDF
 import sys
 import pygame
 
+class AlienInvasion:
+    '''Manages the game and create resources'''
 
+    def __init__(self):
+        '''Initialized the game'''
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption('Alien Invasion')
+
+
+    def run_game(self):
+        '''Control the loop for the game'''
+        while True:
+            #Look for keyboard and mouse events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()    
 
 
 if __name__ == '__main__':
-    #run the game
-    pass
+    ai = AlienInvasion()
+    ai.run_game()
