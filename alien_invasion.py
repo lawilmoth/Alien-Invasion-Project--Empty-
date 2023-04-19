a6f85065f3d11f2151b83dc64c889e0f55256d04
lawilmoth <lwilmoth@eriesd.org>
2023-04-19 09:16:31 -0400

    Game is full screen, the ship can't go off screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,7 +16,9 @@ class AlienInvasion:
         '''Initialized the game'''
         pygame.init()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))   
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         self.ship = Ship(self)
         pygame.display.set_caption('Alien Invasion')
 
@@ -35,18 +37,10 @@ class AlienInvasion:
                 sys.exit()
 
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    #Move ship right
-                    self.ship.moving_right = True
-                if event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)
 
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    #Move ship right
-                    self.ship.moving_right = False
-                if event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
 
     def _update_screen(self):
         '''Updates images and flips the screen'''
@@ -54,6 +48,24 @@ class AlienInvasion:
         self.ship.blitme()  
         #displays the screen
         pygame.display.flip()
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            #Move ship right
+            self.ship.moving_right = True
+        if event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        if event.key == pygame.K_q:
+            sys.exit()
+
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            #Move ship right
+            self.ship.moving_right = False
+        if event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
 
 if __name__ == '__main__':
     ai = AlienInvasion()
--- ship.py
+++ ship.py
@@ -18,14 +18,17 @@ class Ship:
         self.moving_right = False
         self.moving_left = False
         
+        self.settings = ai_game.settings
+        self.x = float(self.rect.x)
 
     def update(self):
         '''Update the ship's position'''
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
-    
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x
+
     def blitme(self): 
         '''Draws the ship on the screen'''
         self.screen.blit(self.image, self.rect)
