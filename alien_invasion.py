8813e04c5ec4b404078778a7db572a987b90af1f
lawilmoth <lwilmoth@eriesd.org>
2023-04-20 10:14:06 -0400

    Chapter 12 done

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,6 +8,8 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
+
 
 class AlienInvasion:
     '''Manages the game and create resources'''
@@ -20,6 +22,7 @@ class AlienInvasion:
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
         pygame.display.set_caption('Alien Invasion')
 
     def run_game(self): 
@@ -28,8 +31,17 @@ class AlienInvasion:
             #Look for keyboard and mouse events
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
-        
+
+    def _update_bullets(self):
+        self.bullets.update()
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
+
+
     def _check_events(self):  
         '''Check events in the game'''   
         for event in pygame.event.get():
@@ -46,6 +58,8 @@ class AlienInvasion:
         '''Updates images and flips the screen'''
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()  
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         #displays the screen
         pygame.display.flip()
 
@@ -57,14 +71,24 @@ class AlienInvasion:
             self.ship.moving_left = True
         if event.key == pygame.K_q:
             sys.exit()
+        if event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
 
-    def _check_keyup_events(self, event):
+    def _check_keyup_events(self, event): 
         if event.key == pygame.K_RIGHT:
             #Move ship right
             self.ship.moving_right = False
         if event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        '''Create a new bullet and add it to the group'''
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+        
 
 
 if __name__ == '__main__':
